86084c6db98349a8fac9244bd158dc98815fa53c
Jake Soenneker <jake@soenneker.com>
2025-08-01 15:32:36 -0500

    tests

---
+++ test/Soenneker.Quark.Table.Tests/QuarkTableTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Soenneker.Quark.Table.Tests
+{
+    public class QuarkTableTests
+    {
+        [Fact]
+        public void Default()
+        { 
+        
+        }
+    }
+}
